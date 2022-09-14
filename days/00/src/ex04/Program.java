d0835ef0937cda0f1afca260c3dc0a07026ee309
iriska <thebluemitsu@gmail.com>
2022-09-14 18:09:45 +0300

    finished ex04

--- days/00/src/ex04/Program.java
+++ days/00/src/ex04/Program.java
@@ -3,33 +3,31 @@ import java.util.*;
 public class Program {
 
     public static void main(String[] args) {
-        HashMap<Character, Integer> map = new HashMap<Char, Integer>();
+        HashMap<Character, Integer> map = new HashMap<>();
 
         Scanner s = new Scanner(System.in);
         String string = s.nextLine();
 
         while(!string.isEmpty()) {
             Character current = string.charAt(0);
-            int count = string.length() - string.replaceAll(String.valueOf(current), "").length();
+            int count = string.length();
+            string = string.replaceAll(String.valueOf(current), "");
+            count -= string.length();
             map.put(current, count);
         }
+        show_res(map);
     }
     public static HashMap<Character, Integer> sortByValue(HashMap<Character, Integer> hm)
     {
         // Create a list from elements of HashMap
         List<Map.Entry<Character, Integer> > list =
-                new LinkedList<Map.Entry<Character, Integer> >(hm.entrySet());
+                new LinkedList<>(hm.entrySet());
 
         // Sort the list
-        list.sort(new Comparator<Map.Entry<Character, Integer>>() {
-            public int compare(Map.Entry<Character, Integer> o1,
-                               Map.Entry<Character, Integer> o2) {
-                return (o1.getValue()).compareTo(o2.getValue());
-            }
-        });
+        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
 
         // put data from sorted list to hashmap
-        HashMap<Character, Integer> temp = new LinkedHashMap<Character, Integer>();
+        HashMap<Character, Integer> temp = new LinkedHashMap<>();
         for (Map.Entry<Character, Integer> aa : list) {
             temp.put(aa.getKey(), aa.getValue());
         }
@@ -37,16 +35,27 @@ public class Program {
     }
     public static void show_res(HashMap<Character, Integer> map) {
         HashMap<Character, Integer> sortedMap = sortByValue(map);
-        Integer max_value= map.values().iterator().next();
-        int i = 0;
-        while(i < 10)
+        Integer max_value= sortedMap.values().iterator().next();
+        int i = 11;
+
+        while(i >= 0)
         {
-            Iterator<Integer> it = map.values().iterator();
-            while(j < 10) {
-                if (it.next() * 10 / max_value == i)
-                    System.out.print("#");
+            Iterator<Integer> it_val = sortedMap.values().iterator();
+            Iterator<Character> it_key = sortedMap.keySet().iterator();
+            int j = 0;
+            while(j < sortedMap.size() && j < 10) {
+                int current_val = it_val.next();
+                int current_key = it_key.next();
+                if ((current_val * 10 / max_value) == i - 1)
+                    System.out.printf("%-2d",current_val);
+                else if(i == 0)
+                    System.out.printf("%-2c", (char)current_key);
+                else if (current_val * 10 / max_value >= i)
+                    System.out.printf("%-2s","#");
+                j++;
             }
-            i++;
+            System.out.println();
+            i--;
         }
     }
 }
