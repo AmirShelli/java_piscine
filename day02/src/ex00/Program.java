fbd2c3fbfadf3dd3e6a81d2b9bb69b571fe1078d
iriska <thebluemitsu@gmail.com>
2022-11-23 10:07:26 +0300

    reread ex04 and 05

--- day02/src/ex00/Program.java
+++ day02/src/ex00/Program.java
@@ -1,11 +1,13 @@
 package ex00;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Program {
-    public static HashMap<String, String> getSignatures(FileInputStream file) {
+    public static HashMap<String, String> getSignatures() throws FileNotFoundException {
+        FileInputStream file = new FileInputStream("/home/mitsu/IdeaProjects/java_piscine/day02/src/ex00/signature.txt");
         HashMap<String, String> signatures = new HashMap<>();
         Scanner fromFile = new Scanner(file);
         while(fromFile.hasNextInt()){
@@ -18,32 +20,37 @@ public class Program {
         }
         return (signatures);
     }
-    public static void byteToHex(byte[] byteArray)
+    public static String byteToHex(byte[] byteArray)
     {
         String hex = "";
         for (byte i : byteArray) {
             hex += String.format("%02X", i);
         }
-        System.out.print(hex);
+        return hex;
     }
-    public static void checkFile(String path) throws FileNotFoundException {
+    public static void checkFile(String path, HashMap<String, String> signatures) throws FileNotFoundException {
         File file = new File(path);
         if(!(file.isFile() || file.exists() || file.canRead()))
             System.out.println("can't open file!");
         else {
             FileInputStream inputFile = new FileInputStream(path);
+            byte[] buffer = new byte[(int) file.length()];
+            String fileSignature = byteToHex(Arrays.copyOfRange(buffer, 0 , 4));
+            System.out.println(fileSignature);
+            //signatures.get(fileSignature);
 
         }
 
     }
     public static void main(String[] args) throws FileNotFoundException {
         FileOutputStream result = new FileOutputStream("result.txt");
+        HashMap<String, String> allSign = getSignatures();
         Scanner in = new Scanner(System.in);
         while(true) {
             String path = in.nextLine();
             if(path.equals("42"))
                 break;
-            checkFile(path);
+            checkFile(path, allSign);
 
         }
     }
