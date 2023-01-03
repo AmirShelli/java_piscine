2b1b08f29796a71e23b374db4be3528f29e803f7
iriska <thebluemitsu@gmail.com>
2023-01-03 20:47:57 +0300

    wip day2/ex02 working with process builder (exploring)

--- day02/src/ex02/Program.java
+++ day02/src/ex02/Program.java
@@ -1,12 +1,36 @@
 package ex02;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Program {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        in.nextLine();
 
+    public static String read(Process p)
+    {
+        try(BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()))){
+            StringBuilder result = new StringBuilder();
+            String s;
+            while ((s = stdInput.readLine()) != null)
+                result.append(s);
+            return result.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void launchLS() throws IOException {
+        ArrayList<String> commands = new ArrayList<String>();
+        commands.add("ls");
+//        commands.add("-l");
+        commands.add("/home/mitsu");
 
+        ProcessBuilder pb = new ProcessBuilder(commands);
+        Process process = pb.start();
+        String result = read(process);
+        System.out.println(result);
+    }
+    public static void main(String[] args) throws IOException {
+        launchLS();
     }
 }
