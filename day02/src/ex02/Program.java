6adb7ad35191a9e9c9d654a1869a1721beaf4c12
iriska <thebluemitsu@gmail.com>
2023-01-03 18:33:47 +0300

    added day03 module finished ex01

---
+++ day02/src/ex02/Program.java
@@ -0,0 +1,12 @@
+package ex02;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.nextLine();
+
+
+    }
+}
---
+++ day03/src/ex00/Program.java
@@ -0,0 +1,7 @@
+package ex00;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
